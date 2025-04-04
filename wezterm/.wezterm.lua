1268dbfb1aa704c49bdcb852cab86ff3fa22546b
Modo Ltunzher <elmodos@users.noreply.github.com>
2025-04-04 13:59:34 +0300

    Wezterm font size

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -4,7 +4,7 @@ local config = wezterm.config_builder()
 config.automatically_reload_config = true
 config.hide_tab_bar_if_only_one_tab = true
 
-config.font_size = 11
+config.font_size = 10
 
 config.window_padding = {
   left = 0,
